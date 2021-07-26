923245906e8dcb7c03e807a20872141763ff6c7e
Wu Andy <wuandy@Wude-MacBook-Pro.local>
2021-07-26 23:06:26 +0800

    自動產生list.

--- scripts.js
+++ scripts.js
@@ -1,18 +1,26 @@
 
-var optionStatus=0
-$(".option").click(function () {
-    if (optionStatus==0) {
-        $(".optionContent").css("display","block");
-        optionStatus=1;
-    } else {
-        $(".optionContent").css("display","none");
-        optionStatus=0;
-    }
+var optionStatus = 0
+$(document).ready(function () {
+    $(".option").click(function () {
+        if (optionStatus == 0) {
+            // $(".optionContent").fadeIn();
+            // $(".optionContent").fadeIn("slow");
+            // $(".optionContent").fadeIn(1000);
+            $(".optionContent").css("display", "block");
+            optionStatus = 1;
+        } else {
+            // $(".optionContent").fadeOut();
+            // $(".optionContent").fadeOut("slow");
+            // $(".optionContent").fadeOut(1000);
+            $(".optionContent").css("display", "none");
+            optionStatus = 0;
+        }
+    })
 });
 
-var bt1=0
-$(".btContent1").click(function(){
-    if (bt1==0) {
+var bt1 = 0
+$(".btContent1").click(function () {
+    if (bt1 == 0) {
         $(".btContent1").removeClass("btContentClose");
         $(".btCircle1").removeClass("btCircleClose");
         $(".btStatus1").removeClass("btStatusClose");
@@ -20,7 +28,7 @@ $(".btContent1").click(function(){
         $(".btCircle1").addClass("btCircleOpen");
         $(".btStatus1").addClass("btStatusOpen");
         $(".btStatus1").text("開");
-        bt1=1;
+        bt1 = 1;
     } else {
         $(".btContent1").removeClass("btContentOpen");
         $(".btCircle1").removeClass("btCircleOpen");
@@ -29,13 +37,13 @@ $(".btContent1").click(function(){
         $(".btCircle1").addClass("btCircleClose");
         $(".btStatus1").addClass("btStatusClose");
         $(".btStatus1").text("關");
-        bt1=0;
+        bt1 = 0;
     }
 });
 
-var bt2=0
-$(".btContent2").click(function(){
-    if (bt2==0) {
+var bt2 = 0
+$(".btContent2").click(function () {
+    if (bt2 == 0) {
         $(".btContent2").removeClass("btContentClose");
         $(".btCircle2").removeClass("btCircleClose");
         $(".btStatus2").removeClass("btStatusClose");
@@ -43,7 +51,7 @@ $(".btContent2").click(function(){
         $(".btCircle2").addClass("btCircleOpen");
         $(".btStatus2").addClass("btStatusOpen");
         $(".btStatus2").text("開");
-        bt2=1;
+        bt2 = 1;
     } else {
         $(".btContent2").removeClass("btContentOpen");
         $(".btCircle2").removeClass("btCircleOpen");
@@ -52,6 +60,39 @@ $(".btContent2").click(function(){
         $(".btCircle2").addClass("btCircleClose");
         $(".btStatus2").addClass("btStatusClose");
         $(".btStatus2").text("關");
-        bt2=0;
+        bt2 = 0;
     }
 });
+
+
+// $.getJSON("./transferExcelToJson/ice_break_format.json", function (result) {
+    // console.log(result);
+
+    // for(var i=0;i>9;i++){
+    //     $(".listName1").text(result[i].name);
+    //     console.log(result[i].name);
+    // }
+    // var array = ['小明', '杰倫', '漂亮阿姨', '小美']
+    // for (var i = 0; i < result.length; i++) {
+        // $(".listName1").text(result[0].name);
+        // $(".listContent1").text(result[0].purpose);
+    // }
+// });
+
+var list_html="<div class='list {{listNum}}'><div class='listPic {{listPicNum}}'><img class='listPic {{listPicNum}}' src='{{linkPic}}'></div><div class='listName {{listNameNum}}'>人名收集員</div><div class='listContent {{listContentNum}}'>營造輕鬆的小組氣氛</div></div>";
+
+var linkPic=[
+    
+]
+
+// 選取 DOM：欲插入 a 連結的 DOM 名為 .list
+for (var i=1;i<10;i++){
+    //取代模板位置成資料replace(要取代的,取代成...)
+    var current_item_html=
+        list_html.replace("{{listNum}}","list"+i)
+                 .replace("{{listPicNum}}","listPic"+i)
+                 .replace("{{listNameNum}}","listName"+i)
+                 .replace("{{listContentNum}}","listContent"+i);
+
+    $(".content").append(current_item_html);
+};
