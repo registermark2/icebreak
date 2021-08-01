c9395808413c0e6b68d90b46d44b8dde2b61b2b6
Wu Andy <wuandy@Wude-MacBook-Pro.local>
2021-08-01 20:53:56 +0800

    功能完成，只是速度上有點太慢...

--- 0801/scripts.js
+++ 0801/scripts.js
@@ -34,26 +34,16 @@ $(document).ready(function () {
       $(".optionContent").css("display", "none");
       // $(".optionContent").remove();
       optionStatus = 0;
-
-
-      relist();//重新列舉list
     }
   });
 });
 
 
 // $(function () { if ($("#div").hasClass('on')) { $(this).css("display", "none"); } });
-//勾選的數量
-var checkBox = {
-  cooperation: 0,
-  compete: 0,
-}
-console.log(checkBox.cooperation);
 
-
-// var bt1 = 0, bt2 = 0;
+var bt1 = 0, bt2 = 0;
 function bt1Switch(obj) {
-  if (checkBox.cooperation == 0) {
+  if (bt1 == 0) {
     $(".btContent1").removeClass("btContentClose");
     $(".btCircle1").removeClass("btCircleClose");
     $(".btStatus1").removeClass("btStatusClose");
@@ -61,8 +51,8 @@ function bt1Switch(obj) {
     $(".btCircle1").addClass("btCircleOpen");
     $(".btStatus1").addClass("btStatusOpen");
     $(".btStatus1").text("開");
-    checkBox.cooperation = 1;
-  } else if (checkBox.cooperation == 1) {
+    bt1 = 1;
+  } else if (bt1 == 1) {
     $(".btContent1").removeClass("btContentOpen");
     $(".btCircle1").removeClass("btCircleOpen");
     $(".btStatus1").removeClass("btStatusOpen");
@@ -70,13 +60,12 @@ function bt1Switch(obj) {
     $(".btCircle1").addClass("btCircleClose");
     $(".btStatus1").addClass("btStatusClose");
     $(".btStatus1").text("關");
-    checkBox.cooperation = 0;
+    bt1 = 0;
   }
-
 }
 
 function bt2Swirch() {
-  if (checkBox.compete == 0) {
+  if (bt2 == 0) {
     $(".btContent2").removeClass("btContentClose");
     $(".btCircle2").removeClass("btCircleClose");
     $(".btStatus2").removeClass("btStatusClose");
@@ -84,8 +73,8 @@ function bt2Swirch() {
     $(".btCircle2").addClass("btCircleOpen");
     $(".btStatus2").addClass("btStatusOpen");
     $(".btStatus2").text("開");
-    checkBox.compete = 1;
-  } else if (checkBox.compete == 1) {
+    bt2 = 1;
+  } else if (bt2 == 1) {
     $(".btContent2").removeClass("btContentOpen");
     $(".btCircle2").removeClass("btCircleOpen");
     $(".btStatus2").removeClass("btStatusOpen");
@@ -93,7 +82,7 @@ function bt2Swirch() {
     $(".btCircle2").addClass("btCircleClose");
     $(".btStatus2").addClass("btStatusClose");
     $(".btStatus2").text("關");
-    checkBox.compete = 0;
+    bt2 = 0;
   }
 }
 
@@ -104,72 +93,28 @@ var list_html =
 
 var apiData;
 
-function createList() {
-  $.getJSON("./transferExcelToJson/ice_break_format.json", function (data) {
+$.getJSON("./transferExcelToJson/ice_break_format.json", function (data) {
 
-    console.log(data);
-    // console.log(apiData);
-    apiData = data;
-    for (var i = 0; i < data.length; i++) {
-      //取代模板位置成資料replace(要取代的,取代成...)
-      var current_item_html = list_html
-        // .replace("{{gameID}}", i)
-        // .replace("{{listNum}}", "list" + i + 1)
-        // .replace("{{listPicNum}}", "listPic" + i + 1)
-        // .replace("{{listNameNum}}", "listName" + i + 1)
-        // .replace("{{listPicNum}}", "listPic" + i + 1)
-        // .replace("{{listContentNum}}", "listContent" + i + 1)
-        // .replace("{{gameName}}", data[i].name)
-        // .replace("{{gamePurpose}}", data[i].purpose)
-        // .replace("{{linkPic}}", "./images/" + data[i].imageName + ".jpeg")
-        // .replace("{{gameID}}", i);
-        .replace("{{listNum}}", "list" + i)
-        .replace("{{listPicNum}}", "listPic" + i)
-        .replace("{{listNameNum}}", "listName" + i)
-        .replace("{{listPicNum}}", "listPic" + i)
-        .replace("{{listContentNum}}", "listContent" + i)
-        .replace("{{gameName}}", data[i].name)
-        .replace("{{gamePurpose}}", data[i].purpose)
-        .replace("{{linkPic}}", "./images/" + data[i].imageName + ".jpeg")
-        .replace("{{gameID}}", i);
+  // console.log(data);
+  apiData = data;
+  console.log(apiData);
+  for (var i = 0; i < data.length; i++) {
+    //取代模板位置成資料replace(要取代的,取代成...)
+    var current_item_html = list_html
+      // .replace("{{gameID}}", i)
+      .replace("{{listNum}}", "list" + i + 1)
+      .replace("{{listPicNum}}", "listPic" + i + 1)
+      .replace("{{listNameNum}}", "listName" + i + 1)
+      .replace("{{listPicNum}}", "listPic" + i + 1)
+      .replace("{{listContentNum}}", "listContent" + i + 1)
+      .replace("{{gameName}}", data[i].name)
+      .replace("{{gamePurpose}}", data[i].purpose)
+      .replace("{{linkPic}}", "./images/" + data[i].imageName + ".jpeg")
+      .replace("{{gameID}}", i);
 
-      $(".content").append(current_item_html);
-    }
-
-  })
-};
-createList();
-// function
-// function filterData(){
-//   del_Btn();
-//   // relist
-//   $.getJSON("./transferExcelToJson/ice_break_format.json", function (data) {
-
-
-
-
-//     for (var i = 0; i < data.length; i++) {
-//       //取代模板位置成資料replace(要取代的,取代成...)
-//       var current_item_html = list_html
-//         // .replace("{{gameID}}", i)
-//         .replace("{{listNum}}", "list" + i + 1)
-//         .replace("{{listPicNum}}", "listPic" + i + 1)
-//         .replace("{{listNameNum}}", "listName" + i + 1)
-//         .replace("{{listPicNum}}", "listPic" + i + 1)
-//         .replace("{{listContentNum}}", "listContent" + i + 1)
-//         .replace("{{gameName}}", data[i].name)
-//         .replace("{{gamePurpose}}", data[i].purpose)
-//         .replace("{{linkPic}}", "./images/" + data[i].imageName + ".jpeg")
-//         .replace("{{gameID}}", i);
-
-//       $(".content").append(current_item_html);
-//     }
-//   })
-// }
-
-
-
-
+    $(".content").append(current_item_html);
+  }
+});
 
 
 var contentDetailExist = 0;
@@ -201,17 +146,17 @@ function show_game_detail(obj) {
     "<div class='contentRulesDetail'>" +
     "{{contentRulesDetail}}" +
     "</div>" +
-    // "</div>" +
+    "</div>" +
     "<div class='contentSuggest'>" +
     "<div class='contentSuggestTitle'>建議</div>" +
-    "<div class='contentSuggestDetail'>" + "{{contentSuggestDetail}}" +
+    "<div class='contentSuggestDetail'>" +
+    "{{contentSuggestDetail}}" +
     "</div>" +
-    // "</div>" +
+    "</div>" +
     "<div class='contentVideo'>" +
     //"<iframe class='contentVideoDetail' src='https://www.youtube.com/embed/XN031PuViqI?list=PL2SrkGHjnWcyWbA4MGSeTQVNiFHt1YbjC' frameborder='0' allowfullscreen></iframe>" +
     "</div>" +
-    "<div class='del_btn' onclick='del_Btn()'>X</div>" +
-    "</div>" +
+    "<div class='del_btn' onclick='myFunction()'>X</div>" +
     "</div>";
 
 
@@ -230,71 +175,6 @@ function show_game_detail(obj) {
 
 }
 
-// checkBox.cooperation=1;
-// checkBox.compete=1;
-
-function relist() {
-  //relist
-  var storeList = [];
-  $.getJSON("./transferExcelToJson/ice_break_format.json", function (data) {
-    for (var x = 0; x < data.length; x++) {
-      if (checkBox[data[x].cata] == 1) {
-        // console.log("find it");
-        console.log(data[x].name);
-        storeList.push(x);
-      }
-    }
-
-
-    console.log("長度: " + storeList.length);
-
-    //del
-    if (storeList.length > 0) {
-      del_test();
-      console.log("刪除完了");
-      //repush
-      for (var i = 0; i < storeList.length; i++) {
-
-        console.log("storeList: " + storeList[i]);
-
-        //抓取storeList 裡面真的有filter的game
-        //取代模板位置成資料replace(要取代的,取代成...)
-        var current_item_html = list_html
-          // .replace("{{gameID}}", i)
-          .replace("{{listNum}}", "list" + storeList[i] + 1)
-          .replace("{{listPicNum}}", "listPic" + storeList[i] + 1)
-          .replace("{{listNameNum}}", "listName" + storeList[i] + 1)
-          .replace("{{listPicNum}}", "listPic" + storeList[i] + 1)
-          .replace("{{listContentNum}}", "listContent" + storeList[i] + 1)
-          .replace("{{gameName}}", data[storeList[i]].name)
-          .replace("{{gamePurpose}}", data[storeList[i]].purpose)
-          .replace("{{linkPic}}", "./images/" + data[storeList[i]].imageName + ".jpeg")
-          .replace("{{gameID}}", storeList[i]);
-
-        $(".content").append(current_item_html);
-      }
-    }
-    else {
-      createList();
-    }
-  })
-}
-// relist();
-
-
-function findKey(inputDict, value, compare = (a, b) => a === b) {
-  return Object.keys(inputDict).find(k => compare(inputDict[k], value))
-}
-
-// var val = checkBox.cooperation
-// console.log(findKey(checkBox, val)) // b
-
-// 自定义比较函数，比如结合 lodash 可以
-// findKey({a: 98, b: 'str'}, _.isEqual) // d
-
-
-
-
 
 $('.nav').click(function () {
   // if (contentDetailExist == 1) {
@@ -303,18 +183,7 @@ $('.nav').click(function () {
   // }
 });
 
-function del_Btn(obj) {
+function myFunction(obj) {
 
   $(".contentDetail").remove();
-}
-
-// function del_list() {
-
-// }
-// del_list();
-
-
-function del_test() {
-  $('.list').remove();
-  console.log("789");
 }
