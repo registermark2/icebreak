778b9c49c7e228dd6196e6e1fc658acf94a54f47
Wu Andy <wuandy@Wude-MacBook-Pro.local>
2021-08-09 22:04:37 +0800

    差美化，功能應該都可以

--- scripts.js
+++ scripts.js
@@ -10,7 +10,52 @@ var listTemplate =
     "</div>" +
     "</div>"
 
+
+var checkchanged = 0
+function reList() {
+    var storeList = [];
+    $.getJSON("./transferExcelToJson/ice_break_format.json", function (data) {
+        console.log(data);
+
+        // filter cata
+        for (var x = 0; x < data.length; x++) {
+            // console.log(data[x].cata);
+            if (filterList[data[x].cata] == 1) {
+                // console.log("find it");
+                console.log(data[x].name);
+                storeList.push(x);
+            }
+        }
+
+        console.log(storeList);
+
+        if (storeList.length > 0) {
+            $('#listData').empty();
+            console.log('刪除');
+            for (var i = 0; i < storeList.length; i++) {
+                //取代模板位置成資料replace(要取代的,取代成...)
+                console.log(data[storeList[i]]);
+                var current_item_html = listTemplate
+                    .replace("{{gameName}}", data[storeList[i]].name)
+                    .replace("{{gamePurpose}}", data[storeList[i]].purpose)
+                    .replace("{{linkPic}}", "./imagesss/" + data[storeList[i]].imageName + ".jpeg")
+                    .replace("{{gameID}}", storeList[i]);
+
+                $("#listData").append(current_item_html);
+            }
+            checkchanged = 1;
+        }
+        else if (checkchanged == 1) {
+            $('#listData').empty();
+            createList();
+            checkchanged = 0;
+        }
+    });
+}
+
+
 function createList() {
+    var storeList = [];
     $.getJSON("./transferExcelToJson/ice_break_format.json", function (data) {
         console.log(data);
 
@@ -24,6 +69,7 @@ function createList() {
 
             $("#listData").append(current_item_html);
         }
+
     });
 }
 
@@ -43,36 +89,53 @@ function updateGameDetial(obj) {
 
 
 
-
-// 第一次刷新
-createList();
-
-
-function switchTest(obj){
-    // console.log($(obj).is(':checked'));
-    if($(obj).is(':checked')==true){
-        console.log("開");
-    }
+var filterList = {
+    static: 0,
+    items: 0,
+    remind: 0,
+    strategy: 0,
 }
 
 
+function switchButton(obj) {
+    console.log(obj);
+    var button = new bootstrap.Button(obj);
+    button.toggle();
 
-// $("input")
+    if ($(obj).attr('aria-pressed') === 'true') {
+        if (obj.value == '靜態') {
+            filterList.static = 1;
+        }
+        if (obj.value == '道具') {
+            filterList.items = 1;
+        }
+        if (obj.value == '反思') {
+            filterList.remind = 1;
+        }
+        if (obj.value == '攻略') {
+            filterList.strategy = 1;
+        }
+    }
+    if ($(obj).attr('aria-pressed') === 'false') {
+        if (obj.value == '靜態') {
+            filterList.static = 0;
+        }
+        if (obj.value == '道具') {
+            filterList.items = 0;
+        }
+        if (obj.value == '反思') {
+            filterList.remind = 0;
+        }
+        if (obj.value == '攻略') {
+            filterList.strategy = 0;
+        }
+    }
+
+    // 第一次刷新
+    reList();
+    console.log(filterList);
+}
 
 
-// $(".form-switch").bootstrapSwitch({
-//     onSwitchChange: function (e, state) {
-//         console.log(state);
-//     }
-// });
-
-
-// handleChange: function (event, state) {
-//     console.log(state);
-//     this.setState({storyStatus: state});
-// },
-
-// $('#mySwitch input').bootstrapSwitch({
-//     onText: 'On',
-//     offText: 'Off'
-// });
+// 第一次刷新
+createList();
